f676fa4ab0d772882eb83220c2c8a590c3cbce93
chriddyp <chris@plot.ly>
2018-01-04 14:25:10 -0500

    evenly spaced graphs sketch

---
+++ dash-evenly-spaced-graphs-sketch.py
@@ -0,0 +1,16 @@
+def EvenlySpacedGraphs(figures):
+    return html.Div([
+        dcc.Graph(figure=figure, style={
+            'width': 'calc(100% / {})'.format(len(figures))
+        }, id='graph-{}'.format(i))
+        for (i, figure) in enumerate(figures)
+    ], className='row')
+
+
+app.layout = html.Div([
+    html.Div(id='div-container')
+])
+
+@app.callback(Output('div-container', 'children'), [...])
+def update_graphs(...):
+    return EvenlySpacedGraphs(figures)
